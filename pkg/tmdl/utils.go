be13dbdbc15e347158bc95009f862c91b69e6833
Ravi Suhag <suhag.ravi@gmail.com>
2025-02-12 13:36:21 +0530

    feat(tmdl): add TM frame support for data link

---
+++ pkg/tmdl/utils.go
@@ -0,0 +1,17 @@
+package tmdl
+
+// ComputeCRC calculates a CRC-16 checksum for the given data.
+func ComputeCRC(data []byte) uint16 {
+	var crc uint16 = 0xFFFF
+	for _, b := range data {
+		crc ^= uint16(b) << 8
+		for i := 0; i < 8; i++ {
+			if (crc & 0x8000) != 0 {
+				crc = (crc << 1) ^ 0x1021
+			} else {
+				crc <<= 1
+			}
+		}
+	}
+	return crc
+}
